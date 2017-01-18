148ebce4aed665c2876520896791b9a0e65ad00d
bkd705 <brennenkdenomme@gmail.com>
2017-01-18 14:44:48 -0500

    Updated print message

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@
 public class HelloWorld {
 
     public static void main(String args[]) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! My github name is bkd705!");
 
         System.out.println("Changed some shtuffsss");
     }
